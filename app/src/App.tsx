9379135ad42e42670cc222e9169132f55949b794
Aryan Thatte <aryan.thatte@gmail.com>
2023-03-31 18:45:20 -0400

    added mui

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 export default function App() {
-  return (
-  <>
-  <div>Hello World</div>
-  </>
-  )
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <section>
+                <div onClick={() => setCount(count + 1)}>{count}</div>
+            </section>
+        </>
+    );
 }
